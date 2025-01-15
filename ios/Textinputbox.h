d87d088d7251f0778cfd56d1629b93aa464eab10
Developer <info@ifoxsolutions.com>
2025-01-15 07:55:52 +0530

    deleted data

--- ios/Textinputbox.h
+++
@@ -1,6 +0,0 @@
-
-#import "generated/RNTextinputboxSpec/RNTextinputboxSpec.h"
-
-@interface Textinputbox : NSObject <NativeTextinputboxSpec>
-
-@end
--- src/index.tsx
+++
@@ -1,5 +0,0 @@
-import Textinputbox from './NativeTextinputbox';
-
-export function multiply(a: number, b: number): number {
-  return Textinputbox.multiply(a, b);
-}
